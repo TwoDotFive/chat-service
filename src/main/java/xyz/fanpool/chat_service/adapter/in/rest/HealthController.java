8be0963c06ca0b163b627c04c97b5348f60880d4
csct3434 <csct3434@gmail.com>
2024-10-25 21:33:18 +0900

    refactor: 헬스 체크 API 개선
    - 불필요한 ResponseEntity 생성 제거

--- src/main/java/xyz/fanpool/chat_service/adapter/in/rest/HealthController.java
+++ src/main/java/xyz/fanpool/chat_service/adapter/in/rest/HealthController.java
@@ -1,6 +1,5 @@
 package xyz.fanpool.chat_service.adapter.in.rest;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
 
 	@GetMapping("/chat/health")
-	public ResponseEntity<Void> health() {
-		return ResponseEntity.ok().build();
+	public void health() {
 	}
 }
